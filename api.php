4abd8ddb446528ccab89833bfa124d02861a21d6
Skimst <senftleben007@googlemail.com>
2019-08-24 18:06:14 +0200

    Test

---
+++ api.php
@@ -0,0 +1,11 @@
+<?php
+    $apiurl = "http://api.openweathermap.org/data/2.5/weather";
+    #QUEST 1: Wetter von dieser URL mit dem API key holen.
+    #QUEST 1 TIPP 1: https://openweathermap.org/appid
+    #QUEST 1 TIPP 2: http://docs.php.net/manual/da/function.http-get.php
+    #how is the weather wher i am pls vertell me  
+
+    #QUEST 2: Wetter daten in der HTML datei anzeigen.
+    #QUEST 2 TIPP 1: API mit /api.php aufrufen.
+    #QUEST 2 TIPP 2: JQUERY GET Request.
+?>
